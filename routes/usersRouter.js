d802b315d886026dd3a88f5ca0c3d7df8fd68bac
Daniel Blanco <dan@danblanco.com>
2020-06-17 21:09:45 -0400

    reformatting the controllers

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -25,13 +25,13 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
-router.route('/').get((req, res) =>{
+router.route('/users/').get((req, res) =>{
     User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('error:  ', err));
 });
 
-router.route('/add').post((req, res) =>{
+router.route('/users/add').post((req, res) =>{
     const username = req.body.username;
 
     const newUser = new User({username});
