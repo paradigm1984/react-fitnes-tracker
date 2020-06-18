c7e3af4567a1511959e81ded00ae09e7f26892a6
Daniel Blanco <dan@danblanco.com>
2020-06-17 20:09:18 -0400

    reformatting the controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,29 +1,24 @@
 // usersControllers.js
 
-const User = require('../models/user.model');
+const User = require('../../models/user.model');
 
 module.exports = {
-// TO DISPLAY ALL USERS 
+
  findAll: function (req, res) {
- User.find()
-  .then(users => res.json(users))
-   .catch(err => res.status(400).json('error:  ', err));
-  },
-  // TO CREATE A NEW USER 
-  create: function (req, res) {
-   
-  // const username = req.body.username;
-  // const newUser = new User({username});
-
-  // newUser.save()
-  // .then(() => res.json('User Added!!'))
-  // .catch(err => res.status(400).json('Error:  ', err));
-
-  // === HOW IT WAS === //
+  User.find(req.query)
+   .then(users => res.json(users))
+   .catch(err => res.status(422).json(err));
+ },
+ findById: function (req, res) {
+  User.findById(req.params.id)
+   .then(user => res.json(user))
+   .catch(err => res.status(422).json(err));
+ },
+ create: function (req, res) {
   User.create(req.body)
    .then(newUser => res.json(newUser))
    .catch(err => res.status(422).json(err));
-  },
+ },
  update: function (req, res) {
   User.findOneAndUpdate({ _id: req.params.id }, req.body)
    .then(user => res.json(user))
