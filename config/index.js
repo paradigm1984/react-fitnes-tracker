942a9af6c56953952de9bebe449e38024d9bb1cf
Daniel Blanco <dan@danblanco.com>
2020-06-18 18:08:51 -0400

    fixing errors

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 // config/index.js
 
 const dbUser = `blanco`;
-const dbPassword = `blanco1984`;
+const dbPassword = `blanco1`;
 
-const MONGODB_URI = `mongodb://${dbUser}:${dbPassword}@ds161104.mlab.com:61104/react-fitness-tracker`;
+const MONGODB_URI = `mongodb://${dbUser}:${dbPassword}@ds111963.mlab.com:11963/react-fitness-tracker`;
 
 module.exports = MONGODB_URI;
 
